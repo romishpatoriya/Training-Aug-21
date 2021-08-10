69494f9ec81ffb3de20147d008fb2f9762bf2f12
Romish Patoriya <patoriyaromish@gmail.com>
2021-08-11 01:27:33 +0530

    Add and drop foreign key

--- SQL/Romish Patoriya/Day1/fk_job_id.sql
+++ SQL/Romish Patoriya/Day1/fk_job_id.sql
@@ -6,3 +6,8 @@ EndDate Date check (EndDate LIKE '--/--/----'),
 Job_Id int CONSTRAINT fk_job_id FOREIGN KEY REFERENCES Jobs(JobId),
 Department_Id int 
 )
+
+/* for drop the existing foreign key fk_job_id from JobHistory */
+
+ALTER TABLE JobHistory
+DROP CONSTRAINT fk_job_id
