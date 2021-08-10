7126ab7523b910f7aa9f144545ac171327cf9fc4
Romish Patoriya <patoriyaromish@gmail.com>
2021-08-10 20:33:50 +0530

    Employee and departments table created

---
+++ SQL/Romish Patoriya/Day1/Employees.sql
@@ -0,0 +1,19 @@
+CREATE TABLE Employees
+(
+Employee_Id int primary key,
+FirstName varchar(20),
+LastName varchar(20),
+Email varchar(25),
+PhoneNumber numeric(10),
+Hire_Date Date,
+JobId int FOREIGN KEY (JobId) REFERENCES Jobs(JobId),
+Salary money,
+Commission float,
+Manager_Id int ,
+Department_Id int CONSTRAINT fkDepartmentId FOREIGN KEY REFERENCES Departments(Department_Id)
+)
+
+CREATE TABLE Departments(
+Department_Id int primary key,
+Department_Name varchar(20) not null
+)
